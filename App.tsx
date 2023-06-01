301e2b8db60f8b2c7f1221baad31fba6d7c91ef5
wjx99 <1184319223@qq.com>
2023-06-01 15:40:17 +0800

    feat:添加导航栏

--- App.tsx
+++ App.tsx
@@ -1,8 +1,12 @@
 //import liraries
 import React from 'react';
-import {View, Text, StyleSheet, NativeModules} from 'react-native';
+import {NativeModules} from 'react-native';
 import Bluetooth from './Bluetooth';
-// create a component
+import HomePage from './homepage';
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+
+const Tab = createBottomTabNavigator();
 const MyClass = () => {
   const cheroModule = NativeModules.CheroModule;
   cheroModule.initialize();
@@ -10,22 +14,15 @@ const MyClass = () => {
     console.log('mydebug:', a);
   });
   return (
-    <View style={styles.container}>
-      <Bluetooth />
-      <Text>MyClass</Text>
-    </View>
+    <>
+      <NavigationContainer>
+        <Tab.Navigator>
+          <Tab.Screen name="Home" component={HomePage} />
+          <Tab.Screen name="Settings" component={Bluetooth} />
+        </Tab.Navigator>
+        {/* Rest of your app code */}
+      </NavigationContainer>
+    </>
   );
 };
-
-// define your styles
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#2c3e50',
-  },
-});
-
-//make this component available to the app
 export default MyClass;
